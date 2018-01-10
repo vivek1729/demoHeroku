12360e2feb337e64a9ebf903ba980c04295e08c7
vivek1729 <sasuke.vivek@gmail.com>
2018-01-09 18:56:36 -0800

    Added initial server code

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/find', function(req, res) {
+  var user_collection = req.app.get('user_collection');
+  var users = user_collection.find({});
+  res.send({'users':users, 'status':'OK', 'count':user_collection.count()});
+});
+
+router.post('/create', function(req, res) {
+  var user_collection = req.app.get('user_collection');
+  var user_obj = req.body;
+  user_obj['timestamp'] = Math.floor(Date.now() / 1000);
+  user_collection.insert(user_obj);
+  res.send({'status':'OK', 'count':user_collection.count()});
+});
+
+module.exports = router;
